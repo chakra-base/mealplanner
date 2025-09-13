cbc4e4d167154bf210301cfa7405847cb0742702
chakra-base <chakkuconnects@gmail.com>
2025-09-13 11:29:34 +0530

    updated gitignore and server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from the server!' });
+})
+
+app.listen(3001,'localhost', () => {
+console.log("Server is running at http://localhost:3001");
+}
+
+)
